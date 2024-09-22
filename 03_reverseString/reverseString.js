69f19d35009432febe0611594832cb4390ed1a70
TableStar <afraroad@gmail.com>
2024-09-22 17:44:04 +0700

    finish ex-03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (words) {
+  const splittedArr = words.split("");
+  let newArr = []
+  for (let i = splittedArr.length-1; i >= 0; i--) {
+    newArr.push(splittedArr[i])
+  }
+  return newArr.join("")
 };
 
 // Do not edit below this line
